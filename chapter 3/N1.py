b7f62ea9aa67e734841abfa295b35d281e0debbc
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-09-25 20:10:15 +0900

    첫 번째 문제

--- chapter 3/N1.py
+++ chapter 3/N1.py
@@ -1,3 +1,20 @@
+"""
+##문제 설명
+정수 2개를 입력 받아서 두 수의 덧셈, 뺄셈, 곱셈, 나눗셈, 몫, 나머지를 구하는 프로그램을 작성해보자.
+
+##입력 예시
+First Number : 10
+Second Number : 4
+
+##출력 예시
+Add : 14
+Sub : 6
+Mul : 40
+Div : 2.5
+Quo : 2
+Rem : 2
+"""
+
 a = int(input("First Number : "))
 b = int(input("Second Number : "))
 print("Add : ",(a + b))
