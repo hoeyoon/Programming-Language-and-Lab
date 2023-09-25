587cedec1c3ce8468dbde59e04c8e1bc81e44182
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-09-25 20:19:56 +0900

    다섯 번째 문제

--- chapter 3/N5.py
+++ chapter 3/N5.py
@@ -1,3 +1,16 @@
+"""
+## 문제 설명
+초(second)를 입력 받아서 시간(hour), 분(minute), 초(second)의 단위로 출력하는 프로그램을 작성해보자.
+
+## 입력 예시
+Number : 10000
+
+## 출력 예시
+Hour : 2
+Minute : 46
+second : 40
+"""
+
 s = int(input("Number : "))
 sec = s % 60
 hour = s / 3600
