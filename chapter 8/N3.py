240cca48ef5905eecdf7e679f8e1a3e81612ea04
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-10-30 18:20:08 +0900

    세 번째 문제

--- chapter 8/N3.py
+++ chapter 8/N3.py
@@ -1,3 +1,16 @@
+'''
+## 문제 설명
+한 학생의 이름과 국어, 영어, 수학, 과학 점수를 차례대로 입력받는다. 학생의 총점과 평균을 구하는 프로그램을 작성해보자.
+- 총점과 평균을 구하는 메소드(멤버 함수)를 구현한다.
+- 강의자료에 있는 코드를 활용하여 클래스 기반으로 작성한다.
+
+## 입력 예시 :
+Hong Gil Dong	# 이름 입력
+100 90 80 70	# 점수 순서대로 입력
+
+## 출력 예시 :
+[Hong Gil Dong] Total : 340, Average : 85.0
+'''
 class Student:
   def __init__(self, name, k, e, m, s):
     self.name = name
@@ -10,11 +23,11 @@ class Student:
     return self.name
 
   def getTotalScore(self):
-    self.avg = self.k + self.e + self.m + self.s
-    return self.avg
-  
+    avg = self.k + self.e + self.m + self.s
+    return avg
+
   def getAvgScore(self):
-    return self.avg / 4
+    return self.getTotalScore() / 4
 
 name = input()
 k, e, m, s = map(int, input().split())
