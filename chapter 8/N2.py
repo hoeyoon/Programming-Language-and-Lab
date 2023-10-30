4e674c59e20191372e8cfc75fcee923d93ffefe2
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-10-30 18:18:13 +0900

    두 번째 문제

--- chapter 8/N2.py
+++ chapter 8/N2.py
@@ -1,3 +1,16 @@
+'''
+## 문제 설명
+초(second)를 입력받아서 시간(hour), 분(minute), 초(second)의 단위로 출력하는 프로그램을 작성해보자.
+- 강의자료에 있는 코드를 활용하여 클래스 기반으로 작성한다.
+
+## 입력 예시 :
+10000
+
+## 출력 예시 :
+Hour : 2
+Minute : 46
+Second : 40
+'''
 class Time:
   def __init__(self, t):
     self.h = t // 3600
